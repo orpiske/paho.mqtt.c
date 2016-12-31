1be9719da369cd456eb776794f580777b86eaa20
Otavio Rodolfo Piske <opiske@redhat.com>
2016-12-31 15:46:07 +0100

    Added RPM spec for Paho C

---
+++ dist/genmock.sh
@@ -0,0 +1,2 @@
+mock -r fedora-25-x86_64 --scm-enable --scm-option method=git --scm-option package=gru --scm-option git_get=set --scm-option spec=dist/gru.spec --scm-option branch=master --scm-option write_tar=True --scm-option git_get='git clone https://github.com/orpiske/gru.git gru'
+
